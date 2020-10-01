c5735c7ce12e74a10c003a3366a544e7b1cf99cc
Hitesh Chopra <34310832+hiteshchopra11@users.noreply.github.com>
2020-10-01 17:35:49 +0530

    Update BarisalDetails.java

--- app/src/main/java/io/zaman/touristbd/BarisalDetails.java
+++ app/src/main/java/io/zaman/touristbd/BarisalDetails.java
@@ -20,40 +20,43 @@ public class BarisalDetails extends AppCompatActivity {
 
         int index = getIntent().getExtras().getInt("key");
         String[] placeName = getResources().getStringArray(R.array.barisal_place_name);
-        if (index == 0){
-            placeTitleDetails.setText(placeName[0]);
-            imageView.setImageResource(R.drawable.barisal);
-            placeContentDetails.setText(R.string.barisal_all_place);
-        }
-        else if(index == 1){
-            placeTitleDetails.setText(placeName[1]);
-            imageView.setImageResource(R.drawable.kukata);
-            placeContentDetails.setText(R.string.sagar_konna);
-        }
-        else if(index == 2){
-            placeTitleDetails.setText(placeName[2]);
-            imageView.setImageResource(R.drawable.durgasa);
-            placeContentDetails.setText(R.string.durga_sagar);
-        }
-        else if(index == 3){
-            placeTitleDetails.setText(placeName[3]);
-            imageView.setImageResource(R.drawable.gutaiyamosjig);
-            placeContentDetails.setText(R.string.gutiya_mosjic);
-        }
-        else if(index == 4){
-            placeTitleDetails.setText(placeName[4]);
-            imageView.setImageResource(R.drawable.meyabarimosjid);
-            placeContentDetails.setText(R.string.meia_bari);
-        }
-        else if(index == 5){
-            placeTitleDetails.setText(placeName[5]);
-            imageView.setImageResource(R.drawable.shaplagram);
-            placeContentDetails.setText(R.string.shapla_gram);
-        }
-        else if(index == 6){
-            placeTitleDetails.setText(placeName[6]);
-            imageView.setImageResource(R.drawable.oxfordmission);
-            placeContentDetails.setText(R.string.oxford_mission);
+
+        switch(index){
+            case 0:
+                placeTitleDetails.setText(placeName[0]);
+                imageView.setImageResource(R.drawable.barisal);
+                placeContentDetails.setText(R.string.barisal_all_place);
+                break;
+            case 1:
+                placeTitleDetails.setText(placeName[1]);
+                imageView.setImageResource(R.drawable.kukata);
+                placeContentDetails.setText(R.string.sagar_konna);
+                break;
+            case 2:
+                placeTitleDetails.setText(placeName[2]);
+                imageView.setImageResource(R.drawable.durgasa);
+                placeContentDetails.setText(R.string.durga_sagar);
+                break;
+            case 3:
+                placeTitleDetails.setText(placeName[3]);
+                imageView.setImageResource(R.drawable.gutaiyamosjig);
+                placeContentDetails.setText(R.string.gutiya_mosjic);
+                break;
+            case 4:
+                placeTitleDetails.setText(placeName[4]);
+                imageView.setImageResource(R.drawable.meyabarimosjid);
+                placeContentDetails.setText(R.string.meia_bari);
+                break;
+            case 5:
+                placeTitleDetails.setText(placeName[5]);
+                imageView.setImageResource(R.drawable.shaplagram);
+                placeContentDetails.setText(R.string.shapla_gram);
+                break;
+            case 6:
+                placeTitleDetails.setText(placeName[6]);
+                imageView.setImageResource(R.drawable.oxfordmission);
+                placeContentDetails.setText(R.string.oxford_mission);
+                break;
         }
     }
 }
